a3c9e24440a2d3efba62c8b07a7442670063ef41
ArtMejia <arteniomejia.student@careerdevs.com>
2022-06-13 23:56:23 -0400

    mosh beginner tutorial from YT

---
+++ src/com/mejia/mosh/Main.java
@@ -0,0 +1,19 @@
+package com.mejia.mosh;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // int age = 30; int - numbers without decimals    = to initialize variable
+        // age = 35;
+        // int myAge = 30;
+        // int herAge = myAge;
+        // byte age = 30;
+        // int viewsCount = 123_456_789; // underscore work like commas
+        // long viewsCount = 3123456789L; // capital L as suffix for large numbers
+        // double price = 10.99;
+        // float price = 10.99F; // capital F as suffix for float
+        // char letter = 'A'; char is for 1 character
+        boolean isEligible = true;
+        System.out.println(isEligible);
+    }
+}
