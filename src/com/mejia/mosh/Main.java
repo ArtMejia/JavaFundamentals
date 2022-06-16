69b250dba10688eede1ec376f2206e4f9d882b22
ArtMejia <arteniomejia.student@careerdevs.com>
2022-06-16 17:37:20 -0400

    mosh beginner tutorial from YT

--- src/com/mejia/mosh/Main.java
+++ src/com/mejia/mosh/Main.java
@@ -1,5 +1,9 @@
 package com.mejia.mosh;
 
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -13,7 +17,44 @@ public class Main {
         // double price = 10.99;
         // float price = 10.99F; // capital F as suffix for float
         // char letter = 'A'; char is for 1 character
-        boolean isEligible = true;
-        System.out.println(isEligible);
+        // boolean isEligible = true;
+        // System.out.println(isEligible);
+        // Date now = new Date();
+        // System.out.println(now);
+        // byte x = 1;  // primitive type store simple values
+        // byte y = x;
+        // x = 2;
+        // System.out.println(x); // x returns 2, y returns 1 . primitive type are completely independent of each other
+        // Point point1 = new Point(1, 1); // reference type store complex objects
+        // Point point2 = point1;
+        // point1.x = 2;
+        // System.out.println(point2); // reference type can influence each other
+        // String message = "  Hello   World" + "!!   ";  // string is reference type but no 'new' operator needed. string literal
+        // System.out.println(message.length()); // find the length f string
+        // System.out.println(message.indexOf("e")); // find index of letter
+        // System.out.println(message.replace("!", "*")); // replace character
+        // System.out.println(message.toLowerCase()); // change all to lowercase
+        // System.out.println(message.trim()); // removes extra spacing at begin and end
+        // String message = "Hello \"Mosh\""; // use a backslash \ to excape them and  add quotes to string
+        // String mwssage = "c:\\Windows\\...."; // escape a a backlash
+        // String message = "c:\nWindows\\...."; // /n to add new line to string
+        // String message = "c:\tWindows\\...."; // /t to add tab to string
+        // System.out.println(message);
+        // int[] numbers = { 2, 3, 5, 1, 4 }; // creating an array. arrays have a fixed length
+        // Arrays.sort(numbers);
+        // System.out.println(Arrays.toString(numbers)); // getting length of array
+        // int[][] numbers = { { 1, 2, 3}, {  4, 5, 6} }; // multidimensional arrays
+        // System.out.println(Arrays.deepToString(numbers)); // deepToString method for multi array
+        //  final float PI = 3.14F; // final variable or constant cannot be reinitialized. use capital letters to initialized constant varibles
+        // int result = 10 / 3; // results of division is a whole number
+        // double result = (double)10 / (double)3;
+        // int x = 1;
+        // int y = ++x;
+        //x++;     // increment operator. postfix or prefix
+        // int x = 1;
+        // x += 2; // augmented operators x -= 2, x *= 2, x /= 2
+        int x = (10 + 3) * 2;
+        System.out.println(x);
+
     }
 }
