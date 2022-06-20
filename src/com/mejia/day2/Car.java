baef0cda391da37558765e1483813b695f15f2ee
ArtMejia <arteniomejia.student@careerdevs.com>
2022-06-19 21:21:12 -0400

    car company and speed method

---
+++ src/com/mejia/day2/Car.java
@@ -0,0 +1,31 @@
+package com.mejia.day2;
+
+public class Car {
+
+    private String company;
+    private int speed;
+
+    public Car (String company, int speed) {
+        this.company = company;
+        this.speed = speed;
+    }
+    public String getCompany() {
+        return company;
+    }
+    public Integer getSpeed() {
+        return speed;
+    }
+
+
+//    public void setCompany() {
+//        this.company = company;
+//    }
+//
+//    public void setSpeed() {
+//        this.speed = speed;
+//    }
+
+
+
+
+}
---
+++ src/com/mejia/day2/Main.java
@@ -0,0 +1,13 @@
+package com.mejia.day2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car typeOfCar = new Car("Honda", 100);
+        System.out.println( typeOfCar.getCompany() );
+        System.out.println( typeOfCar.getSpeed() );
+        System.out.println( typeOfCar.getCompany() + " car's speed is " + typeOfCar.getSpeed() + "km/hr");
+    }
+
+
+}
