1cf10fb0895affb264cc106c797763f8b3efd741
ArtMejia <arteniomejia.student@careerdevs.com>
2022-07-18 21:36:15 -0400

    warmup algorithms in Java

---
+++ src/com/mejia/warmup/WarmUpSet2Bonus1.java
@@ -0,0 +1,21 @@
+package com.mejia.warmup;
+
+public class WarmUpSet2Bonus1 {
+    public static void main(String[] args) {
+
+    }
+
+}
+
+//Create a function that takes an integer n and reverses it.
+//
+//Examples
+//rev(5121) ➞ "1215"
+//
+//rev(69) ➞ "96"
+//
+//rev(-122157) ➞ "751221"
+
+
+
+
