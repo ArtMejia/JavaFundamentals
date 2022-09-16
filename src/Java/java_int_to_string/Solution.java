95ee5ab3d63744457f6c286d563602488e059d4f
ArtMejia <arteniomejia.student@careerdevs.com>
2022-09-15 20:01:10 -0400

    HackerRank 30 Days Of Code

---
+++ src/Java/java_int_to_string/Solution.java
@@ -0,0 +1,13 @@
+package Java.java_int_to_string;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        String str = String.valueOf(n);
+        System.out.println("Good job");
+    }
+}
